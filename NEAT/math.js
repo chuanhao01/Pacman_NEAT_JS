fa1e8ae41fece7a65d88d4b5e35109ecaed24e52
chuanhao01 <chuanhaolim@gmail.com>
2020-01-03 15:43:22 +0800

    Added softmax function

--- NEAT/math.js
+++ NEAT/math.js
@@ -7,3 +7,16 @@ function sigmoidActivation(x){
 function randomNumber(min, max) {
     return (Math.random() * (max - min)) + min;
 }
+
+function softMax(inputs){
+    let total = 0,
+    outputs = [];
+    for(let input of inputs){
+        total += ((Math.E) ** input);
+    }
+    for(let i=0; i<inputs.length; i++){
+        let new_value = (((Math.E) ** inputs[i]) / total);
+        outputs.push(new_value);
+    }
+    return outputs;
+}
